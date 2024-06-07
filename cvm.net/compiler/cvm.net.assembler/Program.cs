58e9178169cedcfde751b54907d5ffb5a9643cb6
Creeper Lv <creeperlv@users.noreply.github.com>
2024-06-08 04:02:23 +1000

    Working on compiler.

---
+++ cvm.net/compiler/cvm.net.assembler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cvm.net.assembler
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
---
+++ cvm.net/compiler/cvm.net.compiler.core/CVMObject.cs
@@ -0,0 +1,18 @@
+﻿using cvm.net.core;
+
+namespace cvm.net.compiler.core
+{
+	public class CVMObject
+	{
+		public Dictionary<string, int> Labels = [];
+		public Dictionary<int, string> Data = [];
+		public List<Instruction> instructions = new List<Instruction>();
+	}
+	public static class IntermediateInstructions
+	{
+		public const uint JMP_LBL = 0x1010;
+		public const uint JF_LBL = 0x1011;
+		public const uint JO_LBL = 0x1012;
+		public const uint CALL_LBL = 0x1014;
+	}
+}
